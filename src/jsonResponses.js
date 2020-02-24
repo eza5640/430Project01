418d1e9ceb88b548fe344148f70d54f0e3e6c971
eza5640 <eza5640@g.rit.edu>
2020-02-24 17:31:48 -0500

    prototype

---
+++ src/jsonResponses.js
@@ -0,0 +1,80 @@
+//should be in a db
+const currentPosts = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getPosts = (request, response) => {
+  const responseJSON = {
+    currentPosts,
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getPostsMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const getPost = (request, response, params) => {
+    //does nothing yet
+};
+
+const addPost = (request, response, params) => {
+    //default json message
+    const responseJSON = {
+      message: 'Title and description are both required.',
+    };
+  
+    //check to make sure we have both fields
+    //We might want more validation than just checking if they exist
+    //This could easily be abused with invalid types (such as booleans, numbers, etc)
+    //If either are missing, send back an error message as a 400 badRequest
+    if (!body.title || !body.description) {
+      responseJSON.id = 'missingParams';
+      return respondJSON(request, response, 400, responseJSON);
+    }
+  
+    //default status code to 201 created
+    let responseCode = 201;
+  
+    //if that user's name already exists in our object
+    //then switch to a 204 updated status
+    if (currentPosts[body.title]) {
+      responseCode = 204;
+    } else {
+      //otherwise create an object with that name
+      users[body.title] = {};
+    }
+  
+    //add or update fields for this user name
+    users[body.title].title = body.title;
+    users[body.title].description = body.description;
+  
+    //if response is created, then set our created message
+    //and sent response with a message
+    if (responseCode === 201) {
+      responseJSON.message = 'Created Successfully';
+      return respondJSON(request, response, responseCode, responseJSON);
+    }
+    // 204 has an empty payload, just a success
+    // It cannot have a body, so we just send a 204 without a message
+    // 204 will not alter the browser in any way!!!
+    return respondJSONMeta(request, response, responseCode);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+module.exports = {
+  addPost,
+  getPost,
+  notFound,
+  getPosts,
+  getPostsMeta,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCss,
+  '/getPost': htmlHandler.getPost,
+  '/getPosts': htmlHandler.getPosts,
+//  '/addPost': jsonHandler.addPost,
+  '/getPostsMeta': jsonHandler.getPostsMeta,
+  '/notFoundMeta': jsonHandler.notFoundMeta,
+  notFound: jsonHandler.notFound,
+};
+
+const handlePost = (request, response, parsedUrl) => {
+    if(parsedUrl.pathname === '/addPost') {
+        const res = response;
+        const body = [];
+        request.on('error', (err) => {
+            console.dir(err);
+            res.statusCode = 400;
+            res.end();
+        });
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            const bodyString = Buffer.concat(body).toString();
+            const bodyParams = query.parse(bodyString);
+            jsonHandler.addPost(request, res, bodyParams);
+        });
+    }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  }
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+// console.log(`Listening on 127.0.0.1: ${port}`);
